18abcac8ffcf38f1686bcc891bc5228143827e8a
Aleksandra <s-petuch@mail.ru>
2022-08-01 09:51:26 +0300

    1st commit

---
+++ src/components/app/App.js
@@ -0,0 +1,30 @@
+import {lazy, Suspense} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import {MainPage, ComicsPage, SingleComicPage} from '../pages';
+import AppHeader from "../appHeader/AppHeader";
+import Spinner from '../spinner/Spinner';
+
+const Page404 = lazy(() => import('../pages/404'));
+
+const App = () => {
+    return (
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Suspense fallback={<Spinner/>}>
+                        <Routes>
+                            <Route path="/" element={<MainPage/>}/>
+                            <Route path="/comics" element={<ComicsPage/>}/>
+                            <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+                            <Route path="*" element={<Page404/>}/>
+                        </Routes>
+                    </Suspense>
+                </main>
+            </div>
+        </Router>
+    )
+}
+
+export default App;
